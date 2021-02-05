2e7e4bd4a20436fe8244551ed5eba0a2aaba6a86
choi <nu940907@naver.com>
2021-02-05 15:28:31 +0900

    1

--- src/main/java/com/example/restfulwebservice/HelloWorldController.java
+++ src/main/java/com/example/restfulwebservice/HelloWorldController.java
@@ -1,9 +1,19 @@
 package com.example.restfulwebservice;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
     // GET
     // /hello-world
+    @GetMapping(path = "/hello-world")
+    public String helloWorld(){
+        return "Hello World";
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("Hello World");
+    }
 }
