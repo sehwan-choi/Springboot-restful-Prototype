d7fcb5436f20e49705a4de2668da33adf0218831
SEB\user <nu940907@gmail.com>
2021-02-06 16:44:45 +0900

    + add UserController 추가   + update 사용자 목록구현

--- src/main/java/com/example/restfulwebservice/helloworld/HelloWorldController.java
+++ src/main/java/com/example/restfulwebservice/helloworld/HelloWorldController.java
@@ -1,6 +1,7 @@
-package com.example.restfulwebservice;
+package com.example.restfulwebservice.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,5 +16,12 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloWorldBean(){
         return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping(path = "/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldBean(@PathVariable String name){
+
+        return new HelloWorldBean(String.format("Hello World! %s", name));
+        //return new HelloWorldBean("Hello World! " + name);
     }
 }
