0645a7420d00864cb7defa7e0082769958e293e5
SEB\user <nu940907@gmail.com>
2021-06-07 01:09:20 +0900

    H2 연동

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user values(1, sysdate(), 'User1', 'test1111', '123456-1234567');
+insert into user values(2, sysdate(), 'User2', 'test2222', '223456-2234567');
+insert into user values(3, sysdate(), 'User3', 'test3333', '323456-3234567');
