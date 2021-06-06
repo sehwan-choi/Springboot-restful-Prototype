6938e7b2c15ebb85e7babc92e788b8ec43aba9fd
SEB\user <nu940907@gmail.com>
2021-06-06 21:53:26 +0900

    HATEOAS 적용

--- src/main/java/com/example/restfulwebservice/user/UserController.java
+++ src/main/java/com/example/restfulwebservice/user/UserController.java
@@ -1,12 +1,16 @@
 package com.example.restfulwebservice.user;
 
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @RestController
 public class UserController {
@@ -22,14 +26,25 @@ public class UserController {
     }
 
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable int id){
+    public Resource<User> retrieveUser(@PathVariable int id){
         User user = service.findOne(id);
 
         if(user == null){
             throw new UserNotFoundException(String.format("ID[%s] not found", id));
         }
+        
+        // HATEOAS
 
-        return user;
+        Resource<User> resource = new Resource<>(user);
+        //ntrollerLinkBuilder linkTo = ControllerLinkBuilder.linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        
+        resource.add(
+                linkTo(methodOn(this.getClass()).retrieveAllUsers()).withRel("all-users"),
+                linkTo(methodOn(this.getClass()).createUser(user)).withRel("create-user"),
+                linkTo(methodOn(this.getClass()).deleteUser(0)).withRel("update-user"),
+                linkTo(methodOn(this.getClass()).updateUser(user)).withRel("delete-user"));
+
+        return resource;
     }
 
     @PostMapping("/users")
@@ -45,19 +60,21 @@ public class UserController {
     }
 
     @DeleteMapping("/users/{id}")
-    public void deleteUser(@PathVariable int id){
+    public ResponseEntity<User> deleteUser(@PathVariable int id){
         User user = service.deleteById(id);
 
         if(user == null) {
             throw new UserNotFoundException(String.format("ID[%s] not found",id));
         }
+        return ResponseEntity.ok(user);
     }
 
     @PutMapping("/users")
-    public void updateUser(@RequestBody User user){
+    public ResponseEntity<User> updateUser(@RequestBody User user){
         User updateUser = service.modifiedById(user);
 
         if(updateUser == null)
             throw new UserNotFoundException(String.format("ID[%s] not found",user.getId()));
+        return ResponseEntity.ok(user);
     }
 }
